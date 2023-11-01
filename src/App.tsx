0acbb10a985c6ab2a4ca83112755fd4d66e5a588
jeremydolle <j.dolle@thecodingmachine.com>
2023-11-01 12:41:50 +0100

    [+] add toaster provider and plug it to the spot form

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import ThemeProvider from '@/theme/ThemeProvider/ThemeProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './translations';
 import { storage } from '@/storage';
+import ToastProvider from '@/provider/ToastProvider';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider storage={storage}>
-      <MainNavigator />
+      <ToastProvider>
+        <MainNavigator />
+      </ToastProvider>
     </ThemeProvider>
   </QueryClientProvider>
 );
