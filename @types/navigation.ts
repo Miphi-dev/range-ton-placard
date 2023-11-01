8c493a8968e8063af0498d3eaa505358465d23c9
Philippine Cluniat <philippine.cluniat@zol.fr>
2023-11-01 10:50:25 +0100

    [*] wip spot form

--- @types/navigation.ts
+++ @types/navigation.ts
@@ -1,8 +1,18 @@
 import { StackScreenProps } from '@react-navigation/stack';
 
-export type ApplicationStackParamList = {
-  Home: undefined;
+export type ApplicationPublicStackParamList = {
+  Login: undefined;
 };
 
-export type ApplicationScreenProps =
-  StackScreenProps<ApplicationStackParamList>;
+export type ApplicationPrivateStackParamList = {
+  Home: undefined;
+  SpotForm: undefined;
+};
+
+export type ApplicationPublicScreenProps<
+  S extends keyof ApplicationPublicStackParamList = keyof ApplicationPublicStackParamList
+> = StackScreenProps<ApplicationPublicStackParamList, S>;
+
+export type ApplicationPrivateScreenProps<
+  S extends keyof ApplicationPrivateStackParamList = keyof ApplicationPrivateStackParamList
+> = StackScreenProps<ApplicationPrivateStackParamList, S>;
