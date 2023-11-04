281fd0f1dc581ac586a789929283c7e5190c1bfa
jeremydolle <j.dolle@thecodingmachine.com>
2023-11-04 10:40:38 +0100

    [+] add global supply search

--- src/components/molecules/Modal/ModalComponent.tsx
+++ src/components/molecules/Modal/ModalComponent.tsx
@@ -6,39 +6,55 @@ import useTheme from '@/theme/useTheme';
 type Props = PropsWithChildren & {
   isVisible?: boolean;
   close: () => unknown;
+  fill?: boolean;
+  direction?: 'top' | 'bottom';
 };
-const ModalComponent = ({ isVisible, close, children }: Props) => {
-  const { layout, gutters } = useTheme();
+const ModalComponent = ({
+  isVisible,
+  close,
+  children,
+  fill,
+  direction,
+}: Props) => {
+  const { layout, gutters, backgrounds } = useTheme();
 
   return (
-    <>
-      <Modal animationType={'slide'} transparent visible={isVisible}>
-        <View style={[layout.flex_1, layout.justifyEnd, layout.relative]}>
-          <TouchableOpacity
-            onPress={close}
-            style={[
-              layout.fullWidth,
-              layout.fullHeight,
-              layout.absolute,
-              { backgroundColor: '#000', opacity: 0.7 },
-            ]}
-          />
-          <View
-            style={[
-              gutters.padding_16,
-              { marginTop: Platform.OS === 'ios' ? 60 : 10 },
-            ]}
-          >
-            {children}
-          </View>
+    <Modal animationType={'slide'} transparent visible={isVisible}>
+      <View
+        style={[
+          layout.flex_1,
+          layout.relative,
+          direction === 'top' ? layout.justifyStart : layout.justifyEnd,
+        ]}
+      >
+        <TouchableOpacity
+          onPress={close}
+          style={[
+            layout.fullWidth,
+            layout.fullHeight,
+            layout.absolute,
+            fill
+              ? backgrounds.purple900
+              : { backgroundColor: '#000', opacity: 0.7 },
+          ]}
+        />
+        <View
+          style={[
+            gutters.padding_16,
+            { marginTop: Platform.OS === 'ios' ? 60 : 10 },
+          ]}
+        >
+          {children}
         </View>
-      </Modal>
-    </>
+      </View>
+    </Modal>
   );
 };
 
 ModalComponent.defaultProps = {
   isVisible: false,
+  fill: false,
+  direction: 'bottom',
 };
 
 export default ModalComponent;
